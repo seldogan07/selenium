bf282eaa2fbf00ae0f48be2b692d9bc86a5d1cb6
Selçuk Doğan <seldogan07@gmail.com>
2023-05-13 19:59:41 +0300

    add6

--- src/test/java/com/myfirstproject/utilities/TestBase.java
+++ src/test/java/com/myfirstproject/utilities/TestBase.java
@@ -1,27 +1,86 @@
 package com.myfirstproject.utilities;
-
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
-
+import java.util.Date;
 public abstract class TestBase {
+    protected static ExtentReports extentReports;
+    protected static ExtentHtmlReporter extentHtmlReporter;
+    protected static ExtentTest extentTest;
+    @BeforeClass
+    public static void extentReportsSetup(){
+//        PATH
+        String now = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        String path = System.getProperty("user.dir")+"/test-output/Reports/"+now+"extent_reports.html";
+//        Create the HTML template using extent html reporter in the path
+        extentHtmlReporter = new ExtentHtmlReporter(path);
+//        Create extent report
+        extentReports = new ExtentReports();
+//        ***********ADDING CUSTOM SYSTEM INFORMATION ***********
+        extentReports.setSystemInfo("Application Name","Apple M1 Unit");
+        extentReports.setSystemInfo("Test Environment","Regression");
+        extentReports.setSystemInfo("Browser","Chrome");
+        extentReports.setSystemInfo("Team Name","Eagles");
+        extentReports.setSystemInfo("SQA","Seren");
+        extentReports.setSystemInfo("Feature Number","FE1056");
+//        ***********DOCUMENT INFORMATION************************
+        extentHtmlReporter.config().setReportName("My Regression Report");
+        extentHtmlReporter.config().setDocumentTitle("My Regression Extent Reports");
+//        ***********DONE WITH CONFIGURATIONS********************
+//        connecting extent reports and extent html reporter
+        extentReports.attachReporter(extentHtmlReporter);
+//        **********CREATE EXTENT TEST THAT IS ALSO KNOWN AS LOGGER**
+        extentTest=extentReports.createTest("MY REGRESSION","MY FIRST EXTENT REPORT");
+    }
+    @AfterClass
+    public static void tearDownClass(){
+//        generate the report
+        extentReports.flush();
+    }
     protected static WebDriver driver;
-
     @Before
     public void setUp(){
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        driver= new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
-
     @After
     public void tearDown() throws InterruptedException {
-        Thread.sleep(3000);
+        Thread.sleep(5000);
         driver.quit();
+    }
+    public void takeScreenshotOfTheEntirePage() throws IOException {
+//        1. TakeScreenShot class with getScreenShotAs method to capture the screenshot
+        File image = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//
+//        2. Create a path to save the image
+//        Create a date for giving dynamic name otherwise the screenshots overrides
+        String now = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());//getting local date in this format
+//                     CURRENT PROJECT FOLDER         foldername   subfoldername imagename
+        String path = System.getProperty("user.dir")+"/test-output/Screenshots/"+now+"image.png";
+//        3. Save the image in the path as a file
+        FileUtils.copyFile(image,new File(path));
+    }
+    public void takeScreenshotOfThisElement(WebElement element) throws IOException {
+        File image=element.getScreenshotAs(OutputType.FILE);
+        String now = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());//getting local date in this format
+        String path = System.getProperty("user.dir")+"/test-output/ElementScreenshots/"+now+"image.png";
+        FileUtils.copyFile(image,new File(path));
     }
 }
